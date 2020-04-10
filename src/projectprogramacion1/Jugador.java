0cfee62b5cf07a40181bc3680b4969121290b2e0
tati01 <56170672+tati01@users.noreply.github.com>
2020-04-10 17:10:15 -0600

    test

--- src/projectprogramacion1/Jugador.java
+++ src/projectprogramacion1/Jugador.java
@@ -4,11 +4,32 @@
  * and open the template in the editor.
  */
 package projectprogramacion1;
-
 /**
  *
  * @author juangutierrez
  */
 public class Jugador {
+    private int coordenadaJugadorX;
+    private int coordenadaJugadorY;
+    private int numeroMinasAlrededor;
+    
+    public static void moverJugador(){   
+        
+    }
+    
+    
+    public static void determinarMinasAlrededor(){   
+    }
+    
+    
+    public static void marcarCoordenadasMina(){   
+    }
+    
+    
+    
+    
+    
+    
+    
     
 }
