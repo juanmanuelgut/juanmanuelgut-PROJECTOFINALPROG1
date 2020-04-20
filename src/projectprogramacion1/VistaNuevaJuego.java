239246dbc80364445fc34619ad8758b47457d675
tati01 <56170672+tati01@users.noreply.github.com>
2020-04-20 17:32:24 -0600

    Avance tati

--- src/projectprogramacion1/VistaNuevaJuego.java
+++ src/projectprogramacion1/VistaNuevaJuego.java
@@ -1,4 +1,3 @@
-
 package projectprogramacion1;
 
 import java.awt.Color;
@@ -27,12 +26,15 @@ import javax.swing.JOptionPane;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author tati
  */
 public class VistaNuevaJuego {
+
+    public static int Vidas = 3;
+    public static int minasPisadas = 0;
+
     public static void iniciar(int dimX, int dimY, int numMinas) {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int screenWidth = (int) (screenSize.width);
@@ -41,56 +43,48 @@ public class VistaNuevaJuego {
         int dimensionX = dimX;
         int dimensionY = dimY;
         int numeroMinas = numMinas;
-        /*int frameWidth = 1000;
-        int frameHeight = 1100;*/
+
         int buttonWidth;
         int buttonHeigth;
         ArrayList<BotonMina> listaBotones = new ArrayList<BotonMina>();
-        
+
         /*buttonWidth = frameWidth / dimensionX;
         buttonHeigth = (frameHeight - 10) / dimensionX;*/
-        
         buttonWidth = screenWidth / dimensionX;
         buttonHeigth = (screenHeight - 10) / dimensionX;
-        
+
         JFrame ventana = new JFrame("Busca Minas");
         JLabel lbl_cantVidasTitulo = new JLabel("Cantidad de vidas restantes: ");
-        JLabel lbl_cantVidas = new JLabel("3");
+        JLabel lbl_cantVidas = new JLabel(Integer.toString(Vidas));
         JLabel lbl_cantMinasPisadasTitulo = new JLabel("Cantidad de minas pisadas");
-        JLabel lbl_cantMinasPisadas = new JLabel("4");
+        JLabel lbl_cantMinasPisadas = new JLabel(Integer.toString(minasPisadas));
         JButton btn_reiniciar = new JButton("Reiniciar juego");
         JPanel mainPanel = new JPanel(new MigLayout("fillx"));
         JPanel pnl_btn = new JPanel(new MigLayout("fillx"));
         JPanel pnl_info = new JPanel(new MigLayout("fillx"));
-        JTextArea historial = new JTextArea("Mi amor 18/4/2020 10:13:00pm\n" +
-                                            "Mi amor 18/4/2020 10:20:00pm\n" +
-                                            "Mi amor 18/4/2020 10:25:00pm",50, 50);
+        JTextArea historial = new JTextArea("Mi amor 18/4/2020 10:13:00pm\n"
+                + "Mi amor 18/4/2020 10:20:00pm\n"
+                + "Mi amor 18/4/2020 10:25:00pm", 50, 50);
         JScrollPane scroll = new JScrollPane(historial);
-        
 
 //        mainPanel.setBackground(Color.red);
 //        pnl_btn.setBackground(Color.LIGHT_GRAY);
 //        pnl_info.setBackground(Color.DARK_GRAY);
-
-
         lbl_cantMinasPisadasTitulo.setFont(new Font("TimesRoman", Font.PLAIN, 25));
         lbl_cantVidasTitulo.setFont(new Font("TimesRoman", Font.PLAIN, 25));
         lbl_cantMinasPisadas.setFont(new Font("TimesRoman", Font.BOLD, 25));
-        
+
         lbl_cantVidas.setForeground(Color.red);
         lbl_cantVidas.setFont(new Font("TimesRoman", Font.BOLD, 25));
-        
-        
-        
+
         btn_reiniciar.setFont(new Font("SerTimesRomanif", Font.PLAIN, 25));
-        
+
         //        Icono para el boton de reiniciar
-         ImageIcon iconoReset = new ImageIcon("C:\\Users\\Mauricio\\Documents\\NetBeansProjects\\PruebaJframeBotones\\img\\reset.png");
-         btn_reiniciar.setIcon(iconoReset);
-        
+        ImageIcon iconoReset = new ImageIcon("C:\\Users\\Mauricio\\Documents\\NetBeansProjects\\PruebaJframeBotones\\img\\reset.png");
+        btn_reiniciar.setIcon(iconoReset);
+
         scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         historial.setEditable(false);
-        
 
         for (int j = 0; j < dimensionY; j++) {
             for (int i = 0; i < dimensionX; i++) {
@@ -139,6 +133,10 @@ public class VistaNuevaJuego {
                                     b.setBackground(Color.red);
                                     b.setOpaque(true);
                                     JOptionPane.showMessageDialog(null, "Perdiste!!!");
+                                    minasPisadas++;
+                                    Vidas = Vidas - 1;
+                                    lbl_cantMinasPisadas.setText(Integer.toString(minasPisadas));
+                                    lbl_cantVidas.setText(Integer.toString(Vidas));
                                     break;
 
                                 } else if (b.getEsJugador()) {
@@ -164,13 +162,12 @@ public class VistaNuevaJuego {
                 });
             }
         }
-        
+
         /*for (int i = 0; i < 50; i++) {
             JButton b;
             b = new JButton("" + i);
             pnl_btn.add(b, "");
         }*/
-
         pnl_btn.setLayout(new GridLayout(dimX, dimY));
         pnl_btn.setSize(200, 500);
 
@@ -184,7 +181,7 @@ public class VistaNuevaJuego {
         pnl_info.add(lbl_cantMinasPisadasTitulo, "align center");
         pnl_info.add(lbl_cantMinasPisadas, "wrap");
         pnl_info.add(btn_reiniciar, "align center, span 2, wrap");
-        pnl_info.add(scroll,"align center");
+        pnl_info.add(scroll, "align center");
 
         ventana.setVisible(true);
         ventana.setSize(screenWidth, screenHeight);
---
+++ src/projectprogramacion1/Vista_Config_Juego.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectprogramacion1;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.*;
+import net.miginfocom.swing.MigLayout;
+
+/**
+ *
+ * @author tati
+ */
+public class Vista_Config_Juego {
+    
+    public static void config() {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        VistaNuevaJuego juego = new VistaNuevaJuego();
+        int screenWidth = (int) (screenSize.width) / 3;
+        int screenHeight = screenSize.height / 3;
+        JFrame ventana = new JFrame("Configuracion de juego");
+        JPanel panel = new JPanel(new MigLayout("fillx"));
+        JLabel nombre_usuario = new JLabel("Nombre de Usuario");
+        JTextField txt_NombreUsuario = new JTextField();
+        JTextField txt_DimensionX = new JTextField();
+        JTextField txt_DimensionY = new JTextField();
+        JTextField txt_cantMinas = new JTextField();
+        JLabel DimensionX = new JLabel("Dimension en X");
+        JLabel DimensionY = new JLabel("Dimension en Y");
+        JLabel cant_minas = new JLabel("Digite la cantidad de minas que desea");
+        JButton btn_iniciar = new JButton("Iniciar");
+        
+        ventana.add(panel);
+        panel.add(nombre_usuario, "align center, wrap");
+        panel.add(txt_NombreUsuario, "align center, wrap, w 50%");
+        panel.add(DimensionX, "align center, wrap");
+        panel.add(txt_DimensionX, "align center, wrap, w 50%");
+        panel.add(DimensionY, "align center, wrap");
+        panel.add(txt_DimensionY, "align center, wrap, w 50%");
+        panel.add(cant_minas, "align center, wrap");
+        panel.add(txt_cantMinas, "align center, wrap, w 50%");
+        panel.add(btn_iniciar, "align center");
+        
+        btn_iniciar.addActionListener((e) -> {
+            String nombre_Usuario = "";
+            int dimX = 0;
+            int dimY = 0;
+            int cantMinas = 0;   
+            nombre_Usuario = txt_NombreUsuario.getText();
+            dimX = Integer.parseInt(txt_DimensionX.getText());
+            dimY = Integer.parseInt(txt_DimensionY.getText());
+            cantMinas = Integer.parseInt(txt_cantMinas.getText());
+            juego.iniciar(dimX, dimY, cantMinas);
+            ventana.dispose();
+        });
+        
+        ventana.setVisible(true);
+        ventana.setSize(screenWidth, screenHeight);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setLocationRelativeTo(null);
+    }
+}
